40e34e426c2e53a9ed8d074a89cd963fead46638
Leslie747401 <leslie747401@gmail.com>
2024-09-03 15:29:43 +0530

    Implemented Google Auth using Supabase

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,11 +9,34 @@ import Google from '@/public/Google.png'
 import { ArrowUpRight } from 'lucide-react';
 import Link from 'next/link';
 import { useState } from 'react';
+import { signInWithGoogle } from '@/lib/auth-actions';
+import { useRouter } from "next/navigation";
+import { createClient } from "@/utils/supabase/client";
+import { signout } from "@/lib/auth-actions";
+import { useEffect } from "react";
 
 
 export default function Navbar() {
 
   const [dropdown,setDropdown] = useState(false);
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+  const supabase = createClient();
+
+  useEffect(() => {
+
+    const fetchUser = async () => {
+      
+        const { data } = await supabase.auth.getUser();
+
+        if(data) {
+            setUser(data?.user?.user_metadata?.avatar_url);
+        }
+    };
+
+    fetchUser();
+
+  }, []);
 
   return (
     <>
@@ -38,28 +61,39 @@ export default function Navbar() {
 
             <div className='flex gap-8 items-center'>
 
-                {/* Sign in Button  */}
-                <button className='bg-white rounded-xl flex justify-center h-fit px-3 sm:px-4 py-2 gap-2'>
-                    <Image
-                        src={Google}
-                        alt='Logo'
-                        width={28}
-                        height={28}
-                        className='object-contain'
-                    />
-                    <p className='text-[16px] sm:text-[18px] font-medium'>Sign in</p>
-                </button>
+                {
+                    user ? 
 
-                {/* Profile Button */}
-                {/* <div className='cursor-pointer' onClick={() => setDropdown(!dropdown)}>
-                    <Image
-                        src={user}
-                        alt='Logo'
-                        width={45}
-                        height={45}
-                        className='rounded-full'
-                    />
-                </div> */}
+                    <>
+                        {/* Profile */}
+                        <button className='cursor-pointer' onClick={() => setDropdown(!dropdown)}>
+                            <Image
+                                src={user}
+                                alt='Logo'
+                                width={45}
+                                height={45}
+                                className='rounded-full'
+                            />
+                        </button>
+
+                    </>
+
+                    :
+
+                    <>
+                        {/* Sign in Button  */}
+                        <button className='bg-white rounded-xl flex justify-center h-fit px-3 sm:px-4 py-2 gap-2' onClick={() => signInWithGoogle()}>
+                            <Image
+                                src={Google}
+                                alt='Logo'
+                                width={28}
+                                height={28}
+                                className='object-contain'
+                            />
+                            <p className='text-[16px] sm:text-[18px] font-medium'>Sign in</p>
+                        </button>
+                    </>
+                }
                 
 
             </div>
@@ -80,7 +114,11 @@ export default function Navbar() {
                     </button>
                 </Link>
 
-                <button className='w-full rounded-xl flex justify-start pl-5 pr-16 py-2'>
+                <button className='w-full rounded-xl flex justify-start pl-5 pr-16 py-2' onClick={() => {
+                    signout();
+                    setUser(null);
+                    setDropdown(false);
+                }}>
                     <p className='text-white text-[16px] sm:text-[18px] font-medium'>Sign out</p>
                 </button>
                 
